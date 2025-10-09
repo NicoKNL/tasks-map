bde378db38b95d3fa8e08756a00f3a9eb1274009
Nico Klaassen <klaassen.nico@gmail.com>
2025-10-09 20:29:40 +0200

    fix: change dataview dependency info message (#106)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { WorkspaceLeaf, Plugin, Notice } from "obsidian";
+import { WorkspaceLeaf, Plugin } from "obsidian";
 
 import TaskMapGraphItemView, { VIEW_TYPE } from "./views/TaskMapGraphItemView";
 
 export default class TasksMapPlugin extends Plugin {
   async onload() {
-    if (!this.checkDataviewPlugin()) {
-      return;
-    }
-
+    // Always register the view - it will handle the Dataview check internally
     this.registerView(
       VIEW_TYPE,
       (leaf: WorkspaceLeaf) => new TaskMapGraphItemView(leaf)
@@ -20,29 +17,6 @@ export default class TasksMapPlugin extends Plugin {
         this.activateViewInMainArea();
       },
     });
-  }
-
-  private checkDataviewPlugin(): boolean {
-    const plugins = (this.app as any).plugins; // eslint-disable-line @typescript-eslint/no-explicit-any
-    const dataviewPlugin = plugins?.plugins?.["dataview"];
-
-    if (!dataviewPlugin) {
-      new Notice(
-        "Tasks Map: Dataview plugin is not installed. Please install and enable the Dataview plugin.",
-        10000
-      );
-      return false;
-    }
-
-    if (!plugins.enabledPlugins?.has("dataview")) {
-      new Notice(
-        "Tasks Map: Dataview plugin is installed but not enabled. Please enable the Dataview plugin.",
-        10000
-      );
-      return false;
-    }
-
-    return true;
   }
 
   async activateViewInMainArea() {
