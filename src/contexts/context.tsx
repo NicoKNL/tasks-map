a4bcbe7f974efd20d24ffc84902f3d94458ea6f5
Nico Klaassen <klaassen.nico@gmail.com>
2025-11-10 17:25:27 +0100

    fix lint issue

--- src/contexts/context.tsx
+++ src/contexts/context.tsx
@@ -5,7 +5,7 @@ export const AppContext = createContext<App | undefined>(undefined);
 
 interface TagsContextValue {
   allTags: string[];
-  updateTaskTags: (taskId: string, newTags: string[]) => void;
+  updateTaskTags: (taskId: string, newTags: string[]) => void; // eslint-disable-line no-unused-vars
 }
 
 export const TagsContext = createContext<TagsContextValue>({
